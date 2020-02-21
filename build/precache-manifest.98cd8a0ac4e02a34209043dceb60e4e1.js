1ae1a99ff1e5f414bf119bd5f07280ec5abc4e65
VyLe <thaovyletruong@gmail.com>
2020-02-20 22:01:23 -0600

    build

---
+++ build/precache-manifest.98cd8a0ac4e02a34209043dceb60e4e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e931d60bb735ce29fea3b11de4dc75ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cfa98212ae8ce06e7e5",
+    "url": "/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "69fb369d30cb4f44ca55",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6cfa98212ae8ce06e7e5",
+    "url": "/static/js/2.a64a49a8.chunk.js"
+  },
+  {
+    "revision": "a3724a873c53ad59e7eef0ea33613b5c",
+    "url": "/static/js/2.a64a49a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fb369d30cb4f44ca55",
+    "url": "/static/js/main.16340650.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
